49b93da632dad34ce07fa7cdd49009dfda11f58f
afonsohbm <afonso.hbm@gmail.com>
2020-12-01 01:58:52 -0300

    Projeto2

--- Projeto2/ProjetoBebela2.R
+++ Projeto2/ProjetoBebela2.R
@@ -1,32 +1,48 @@
 library(httr)
+library(magrittr)
 
-pags <- (1:13)
+#pags <- (1:13)
 
-url <- url2 <- "https://www.jardineiro.net/plantas-de-a-a-z-por-nome-cientifico/"
+url <- "https://www.jardineiro.net/plantas-de-a-a-z-por-nome-cientifico/"
 
-pag <- GET(url = url,
-    query = list(
-      page = 2
-    ))
+pag <- httr::GET(url = url)
 
-index <- pag %>% read_html() %>% xml_find_all("//*[@target='_self']") %>% xml_attr("href")
+#pega o link da página de cada planta
+index <- pag %>% xml2::read_html() %>% xml2::xml_find_all("//*[@target='_self']") %>% xml2::xml_attr("href")
+
+caminho <- "Projeto2/raw/"
+
+if(!fs::dir_exists(caminho)) fs::dir_create(caminho)
 
 
-textoTabela <- index[[1]] %>% read_html() %>% xml_find_all("//*[(@id = 'custom_type_fields')]//li") %>% html_text()
+#baixa a página de cada planta
+index %>% 
+  purrr::map(~GET(.x, 
+                  httr::write_disk(paste0(caminho, 
+                                          which(index == .x)), 
+                                   TRUE)))
 
-# index[[1]] %>% read_html() %>% html_nodes("#custom_type_fields li") %>% html_text()
 
-if (any(!is.na(str_extract(a, "Sinonímia:")))) {
-  a <- a[-2]
+#extração e arrumação dos dados de cada planta
+extraiDados <- function(path){
+  vetor <- path %>% xml2::read_html() %>% xml2::xml_find_all("//*[(@id = 'custom_type_fields')]//li") %>% rvest::html_text()
+  if (any(!is.na(str_extract(vetor, "Sinonímia:")))) {
+    vetor <- vetor[-2]
+  }
+  
+  vetor <- as.data.frame(vetor) %>% 
+    tidyr::separate(col = vetor, into = c("names","dados"), sep = ": ", extra = "merge") %>% 
+    tidyr::pivot_wider(names_from = names, values_from=dados)
+  
+  return(vetor) 
 }
 
-tabela <- as.data.frame(textoTabela) %>%
-  tidyr::separate(textoTabela, into = c("names","dados"), sep = ": ", extra = "merge") %>%
-  dplyr::select(dados) %>%
-  t() %>%
-  as.data.frame()
 
 
-names(tabela) <- names(tabela)
-tabela <- rbind(tabela, tabela2)
+base <- paste0(caminho, c(1:length(fs::dir_ls(caminho)))) %>% 
+  purrr::map_dfr(extraiDados)
+
+
+
+saveRDS(base, file = "Projeto2/base.RDS")
 
